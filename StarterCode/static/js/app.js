09d4ee91494ad7f1fc332d5d406afc3ebdb212a7
Jorge Ignacio Chavez Jr <cooktherook94@jorges-mbp.home>
2021-05-03 18:46:11 -0700

    table data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,19 @@
-// from data.js
+// from data.js assign array of objects to descriptive variable
 var tableData = data;
+// console.log(ufoData);
+// Select the button
+// var dateButton = d3.select("#filter-btn");
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+function table(data){
+    data.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+        table(tableData);
+
+
